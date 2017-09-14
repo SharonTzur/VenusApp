efd3f683ef712b880d9a81ac1059abb55a016a66
SharontTzur <gaytomatoxx@gmail.com>
2017-09-14 18:22:59 +0300

    loader

--- src/app/shared/components/loader/loader.component.ts
+++ src/app/shared/components/loader/loader.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {LoaderService} from "../../../services/loader/loader.service";
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.css']
+  styleUrls: ['./loader.component.scss']
 })
 export class LoaderComponent implements OnInit {
 
-  constructor() { }
+
+  private isLoading: boolean = false;
+  private subscription;
+
+  constructor(private loadService : LoaderService) { }
 
   ngOnInit() {
+    this.subscription = this.loadService.loading$.subscribe(loading=>this.toggle(loading));
+  }
+
+  ngOnDestroy():void {
+    this.subscription.unsubscribe();
+  }
+
+
+  private toggle(loading) {
+    this.isLoading = loading;
   }
 
 }
